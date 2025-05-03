3e78364d813ef54c88fa79ed19e2f63deaed083f
Sante123 <peze875@gmail.com>
2025-05-03 07:45:07 -0700

    recent comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Button from "./components/Button"
+import ButtonGradient from "./assets/svg/ButtonGradient"
 
-
-function App() {
-  
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="mailto:chromeluxegarage@hotmail.com">
+          Contact Us
+        </Button>
+        
+      </div>
+      <ButtonGradient />
+    </>
   )
 }
 
