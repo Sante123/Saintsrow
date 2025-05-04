5174e3aa5c5592070af219249107102ce4495dbe
Sante123 <peze875@gmail.com>
2025-05-04 03:02:28 -0700

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Button from "./components/Button"
 import ButtonGradient from "./assets/svg/ButtonGradient"
+import Header from "./components/Header"
 
 const App = () => {
   return (
@@ -7,10 +8,7 @@ const App = () => {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="mailto:chromeluxegarage@hotmail.com">
-          Contact Us
-        </Button>
-        
+        <Header/>
       </div>
       <ButtonGradient />
     </>
